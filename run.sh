c7ae6d09610b3caa09b3c4d7f3814da45ac4d743
RattyDAVE <dave@pucknell.co.uk>
2019-01-21 18:17:29 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-/usr/bin/ssh-keygen -A
-yes n | ssh-keygen -t rsa -b 4096 -f  /etc/ssh/ssh_host_key -N '' >/dev/null
-
 PATH=$PATH:bin
 
 mkdir dump
 tmux new -s "cray" -d "cray_xmp_sim cos_117.cfg"
+
+/usr/bin/ssh-keygen -A
+yes n | ssh-keygen -t rsa -b 4096 -f  /etc/ssh/ssh_host_key -N '' >/dev/null
 
 /usr/sbin/sshd -D
 
